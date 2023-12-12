618af9e84a4167b93e559d510375463ce773732b
François Billet <francoisbillet@live.fr>
2023-12-12 17:15:57 +0100

    day8: WIP part2

--- advent-of-code-2023/src/main/kotlin/day8/Part2.kt
+++ advent-of-code-2023/src/main/kotlin/day8/Part2.kt
@@ -5,19 +5,21 @@ import java.io.File
 fun main() {
     val (directions, network) = File("src/main/kotlin/day8/puzzle.txt").readText().split("\n\n")
     val networkMap = mutableMapOf<String, Pair<String, String>>()
+    var currentNodes = mutableListOf<String>()
     network.split("\n").forEach {
         val (node, rawDirectionNodes) = it.split(" = ")
+        if (node.endsWith('A')) currentNodes.add(node)
         val directionNodes = rawDirectionNodes.filterNot { it == '(' || it == ')' }.split(", ")
         networkMap[node] = Pair(directionNodes[0], directionNodes[1])
     }
 
-    var count = 0
-    var currentNode = "AAA"
-    while (currentNode != "ZZZ") {
-        val direction = directions[count % directions.length]
-        currentNode = if (direction == 'L') networkMap[currentNode]!!.first else networkMap[currentNode]!!.second
+    var count = 0L
+    while (!currentNodes.all { it.endsWith('Z') }) {
+        val direction = directions[(count % directions.length).toInt()]
+        currentNodes = currentNodes.map{
+            if (direction == 'L') networkMap[it]!!.first else networkMap[it]!!.second
+        }.toMutableList()
         count++
     }
-
     println(count)
 }
