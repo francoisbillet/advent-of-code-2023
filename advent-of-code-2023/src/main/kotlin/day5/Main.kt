cb91790493eee36f94841d7b1a3314931f8525e8
François Billet <francoisbillet@live.fr>
2023-12-12 08:04:13 +0100

    day5: part1

--- advent-of-code-2023/src/main/kotlin/day5/Main.kt
+++ advent-of-code-2023/src/main/kotlin/day5/Main.kt
@@ -5,42 +5,29 @@ import java.io.File
 fun main() {
     val puzzle = File("src/main/kotlin/day5/puzzle.txt").readText()
     val puzzleParts = puzzle.split("\n\n")
-
     val seeds = puzzleParts[0].split(": ")[1].split(" ")
-    val seedToSoilMap = getMap(puzzleParts[1].split(":\n")[1].split("\n"))
-    val soilToFertilizerMap = getMap(puzzleParts[2].split(":\n")[1].split("\n"))
-    val fertilizerToWaterMap = getMap(puzzleParts[3].split(":\n")[1].split("\n"))
-    val waterToLightMap = getMap(puzzleParts[4].split(":\n")[1].split("\n"))
-    val lightToTemperatureMap = getMap(puzzleParts[5].split(":\n")[1].split("\n"))
-    val temperatureToHumidityMap = getMap(puzzleParts[6].split(":\n")[1].split("\n"))
-    val humidityToLocationMap = getMap(puzzleParts[7].split(":\n")[1].split("\n"))
 
+    val maps = puzzleParts.subList(1, puzzleParts.size)
     val results = mutableListOf<Long>()
     seeds.forEach {
-        val seedToSoilResult = getResultInMapForSeed(it.toLong(), seedToSoilMap)
-        val soilToFertilizerResult = getResultInMapForSeed(seedToSoilResult, soilToFertilizerMap)
-        val fertilizerToWaterResult = getResultInMapForSeed(soilToFertilizerResult, fertilizerToWaterMap)
-        val waterToLightResult = getResultInMapForSeed(fertilizerToWaterResult, waterToLightMap)
-        val lightToTemperatureResult = getResultInMapForSeed(waterToLightResult, lightToTemperatureMap)
-        val temperatureToHumidityResult = getResultInMapForSeed(lightToTemperatureResult, temperatureToHumidityMap)
-        val humidityToLocationResult = getResultInMapForSeed(temperatureToHumidityResult, humidityToLocationMap)
-        results.add(humidityToLocationResult)
+        results.add(getLocationForSeed(it.toLong(), maps))
     }
     println(results.min())
 }
 
-fun getResultInMapForSeed(seed: Long, map: Map<Long, Long>): Long = map[seed] ?: seed
-
-fun getMap(puzzleMap: List<String>): Map<Long, Long> {
-    val map = mutableMapOf<Long, Long>()
-    puzzleMap.forEach {
-        val (destinationRangeStart, sourceRangeStart, rangeLength) = it.split(" ")
-        var i = rangeLength.toInt()
-        while (i > 0) {
-            val toAdd = rangeLength.toInt() - i
-            map[sourceRangeStart.toLong() + toAdd] = destinationRangeStart.toLong() + toAdd
-            i--
+fun getLocationForSeed(seed: Long, maps: List<String>): Long {
+    var target = seed
+    maps.forEach {
+        var flag = false
+        val actualMap = it.split(":\n")[1].split(("\n"))
+        actualMap.forEach { line ->
+            val (destinationRangeStart, sourceRangeStart, rangeLength) = line.split(" ")
+            if (target in sourceRangeStart.toLong()..(sourceRangeStart.toLong() + (rangeLength.toLong() - 1)) && !flag) {
+                val delta = target - sourceRangeStart.toLong()
+                target = destinationRangeStart.toLong() + delta
+                flag = true
+            }
         }
     }
-    return map
+    return target
 }
